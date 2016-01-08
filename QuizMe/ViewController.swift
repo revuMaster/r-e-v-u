66ad95d042a10979fc026d5d86bbb455d2ca987a
Escobido, Matthew George O <mescobido@aim-facmb003.local>
2016-01-08 21:17:18 +0800

    Initial Commit

---
+++ QuizMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuizMe
+//
+//  Created by Escobido, Matthew George O. on 08/01/2016.
+//  Copyright © 2016 AIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
